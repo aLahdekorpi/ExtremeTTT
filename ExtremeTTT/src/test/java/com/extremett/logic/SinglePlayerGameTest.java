67cbb269f10e12a358478b9d8c951d64a9dd60a4
Alex Lähdekorpi <lalex@dhcp-asv-70.helsinki.fi>
2016-03-30 10:57:46 +0300

    paketinvaihto

--- ExtremeTTT/src/test/java/com/extremett/logic/SinglePlayerGameTest.java
+++ ExtremeTTT/src/test/java/com/extremett/logic/SinglePlayerGameTest.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.extremett.gui;
+package com.extremett.logic;
 
 import com.extremettt.logic.SinglePlayerGame;
 import org.junit.After;
